7e3f08ddd46a442921937f88efb667cdbd7ed94b
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-11-05 19:46:32 -0300

    d

---
+++ main.js
@@ -0,0 +1,30 @@
+const url =
+  "https://servicodados.ibge.gov.br/api/v3/agregados/5938/periodos/2020/variaveis/543?localidades=N1[all]";
+
+async function visualizarDados() {
+  const res = await fetch(url);
+
+  const dados = await res.json();
+  console.log(dados);
+  regiao = 1;
+
+  resultado_dados = dados[0]["resultados"][0]["series"][regiao]["serie"];
+
+  let ano = Object.keys(resultado_dados);
+  let valor = Object.values(resultado_dados);
+
+  resultado_regiao =
+    dados[0]["resultados"][0]["series"][regiao]["localidade"]["nome"];
+
+  console.log(resultado_dados, resultado_regiao);
+
+  const paragrafo = document.createElement("p");
+  paragrafo.classList.add("graficos--container__texto");
+  paragrafo.innerHTML = `texto ${ano[0]} ${valor[0]}`;
+  console.log(paragrafo);
+  const container = document.getElementById("grafico--container");
+
+  container.appendChild(paragrafo);
+}
+
+visualizarDados();
